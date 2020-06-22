199ead539dfb8a6a5f603442ae24c6a138dfb9b2
zichunguo <guozichunvip@163.com>
2020-06-22 18:24:12 +0800

    修改配置

--- mydubbo-user-provider/src/main/java/com/dubbo/test/Application.java
+++ mydubbo-user-provider/src/main/java/com/dubbo/test/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Application {
 	
 	public static void main(String[] args) throws IOException {
-		ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("classpath:/provider.xml");
+		ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("provider.xml");
 		ioc.start();
 		
 		System.in.read(); // 按任意键退出
